766e0c4e922ad050e14b285f1c4eb9797d20e97a
Kent Chapline <k.chapline@tcu.edu>
2015-06-08 11:47:21 -0500

    Update gphoto2-updater.sh

--- gphoto2-updater.sh
+++ gphoto2-updater.sh
@@ -71,23 +71,23 @@ echo "gphoto2-temp-folder created"
 
 echo 
 echo "-------------------------"
-echo "Downloading libusb 1.0.17"
+echo "Downloading libusb 1.0.19"
 echo "-------------------------"
 echo
 
-if wget -q https://download.nus.edu.sg/mirror/ubuntu/pool/main/libu/libusbx/libusbx_1.0.17.orig.tar.bz2
+if wget -q http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.19/libusb-1.0.19.tar.bz2
 	then
-		tar xjvf libusbx_1.0.17.orig.tar.bz2
-		cd libusbx-1.0.17/
+		tar xjvf libusbx-1.0.19.tar.bz2
+		cd libusb-1.0.19/
 	else
-		echo "Unable to get libusbx_1.0.17"
+		echo "Unable to get libusb_1.0.19"
 		echo "Cleaning and exiting..."
 		exit 1
 fi
 
 echo 
 echo "--------------------------------------"
-echo "Compiling and installing libusb 1.0.17"
+echo "Compiling and installing libusb 1.0.19"
 echo "--------------------------------------"
 
 ./configure
